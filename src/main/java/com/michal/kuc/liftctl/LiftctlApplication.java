adf6116511153b40bfbad385248dfef8778dac11
mkuc-theev <mail.michael.kuc@gmail.com>
2022-12-13 13:12:54 +0100

    Initial commit

---
+++ src/main/java/com/michal/kuc/liftctl/LiftctlApplication.java
@@ -0,0 +1,13 @@
+package com.michal.kuc.liftctl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftctlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftctlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michal/kuc/liftctl/LiftctlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michal.kuc.liftctl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftctlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
